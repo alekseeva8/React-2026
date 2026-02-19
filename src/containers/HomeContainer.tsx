a03b248fcfe7a9f014f656f3941de95f31389ed5
Alekseeva <alekseeva.ev8@gmail.com>
2026-02-19 17:35:55 +0300

    task 3 - excess state removed

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,35 +1,24 @@
 
-import React, {useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { TabStripSelectEventArguments } from '@progress/kendo-react-layout';
 import HomeView, {HomeProps} from "../views/Home";
 
+
 const HomeContainer = () => {
     const pages = [{ title: 'О нас', path: '/about' }, { title: 'Счетчики', path: '/counters' }];
 
-    const [selected, setSelected] = useState(-1);
+    const location = useLocation();
+    const selectedIndex = pages.findIndex(page => page.path === location.pathname);
 
-    //для синхронизации выбранного таба и url (при клике на таб)
+    //при клике на таб снавигировать программно на соответсвующую страницу
     const navigate = useNavigate();
     const handleSelect = (e: TabStripSelectEventArguments) => {
         navigate(pages[e.selected].path);
-        setSelected(e.selected);
     };
-
-    //для синхронизации выбранного таба и url (при ручном вводе url)
-    const location = useLocation();
-    useEffect(() => {
-        const index = pages.findIndex(page => page.path === location.pathname);
-        const isValidPath = index >= 0;
-        if (!isValidPath && location.pathname !== '/') {
-            navigate('/404');
-        }
-        setSelected(isValidPath ? index : -1);
-    }, [location.pathname]);
 
     const props: HomeProps = {
         pages: pages,
-        selectedIndex: selected,
+        selectedIndex: selectedIndex,
         onSelect: handleSelect
     }
 
